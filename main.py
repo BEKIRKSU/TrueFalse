188251f3f8e2bc7f60375238f604ef822b40adb2
BEKIRKSU <bekirkscu@gmail.com>
2023-09-13 15:03:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class User:
 # pass gets rid of the need to fill in the class or function.
+# Instead we have an __init__ which is an initialiser with parameters. Such as id and username.
      def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
@@ -13,5 +14,7 @@ print(user_1.username)
 # When printing be specific.
 
 user_1 = User("007", "Bekir")
+# much easier to make an object with 2 arguments. More convenient.
+
 
 # this prints the id
