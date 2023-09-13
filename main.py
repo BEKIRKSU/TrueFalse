0d4b1d203dba54fba4dfde0cb928d59b45cb74dc
BEKIRKSU <bekirkscu@gmail.com>
2023-09-13 14:58:07 +0100

    user_id and name

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 class User:
-    pass
 # pass gets rid of the need to fill in the class or function.
 
-def __init__(self, user_id, username):
-    self.id = user_id
-    self.username = username
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 #     it's convention that the name of the parameter is equal to the name of the attribute.
 
-user_1 = User()
-user_1.id = "007"
-user_1.username = "Name"
+user_1 = User("001", "Bekir")
+# instead of below, check what's above this line.
+# user_1.id = "007"
+# user_1.username = "Name"
+print(user_1)
 
 print(user_1.id)
 # this prints the id
