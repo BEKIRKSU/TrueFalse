bdaf5fa150396b673e3f2177354c444a05e29359
BEKIRKSU <bekirkscu@gmail.com>
2023-09-13 16:29:28 +0100

    += 1

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ class User:
      def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
-        self.follower = 0
+        self.followers = 0
+        self.following = 0
 #     it's convention that the name of the parameter is equal to the name of the attribute.
+     def follow(self, user):
+         user.followers += 1
+         self.following += 1
+# You use self when writing inside a class.
+
 
 user_1 = User("001", "Bekir")
 # instead of below, check what's above this line.
@@ -19,11 +25,19 @@ print(user_1.username)
 
 user_2 = User("007", "Bekir")
 # much easier to make an object with 2 arguments. More convenient.
-print(user_1.follower)
+print(user_1.followers)
 # This will print '0' although it's not an argument in the initializer. Because the class has a set
 # attribute.
 
 #  attributes and methods. Methods are used to possibly change the attributes.
 # So we'll have a function inside a class (it's called a method when this is the case)
+# Methods look the same as functions, def name(): body.
+# it then get's the object, so 'self' and changes the attribute 'seats' like
+# def name():
+#    self.seats = 2
 
-# this prints the id
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
